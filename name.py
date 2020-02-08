23c73f6f6e63b42f42a8fa343c2f545b9425462d
sulemanali5760 <60712987+sulemanali5760@users.noreply.github.com>
2020-02-09 01:11:15 +0500

    Add files via upload

--- name.py
+++ name.py
@@ -1,1 +1,101 @@
-print("My name is Suleman Ali")
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "My name is Suleman Ali\n"
+     ]
+    }
+   ],
+   "source": [
+    "print (\"My name is Suleman Ali\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "i am 18 years old\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"i am 18 years old\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "i am studing B.S Electrical fron SSUET\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"i am studing B.S Electrical fron SSUET\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "i was born in PAkistan\n"
+     ]
+    }
+   ],
+   "source": [
+    "birthplace=\"PAkistan\"\n",
+    "print(\"i was born in \"+birthplace)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
